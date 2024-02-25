ceec094555f4a39750caabe599784556643bf52d
machip3r <mac_258@hotmail.com>
2024-02-25 02:23:59 -0600

    utils files

---
+++ src/util/translations.js
@@ -0,0 +1,61 @@
+export const language = "en";
+
+export const nav = {
+    en: {
+        home: {
+            text: "HOME",
+            icon: "icon"
+        },
+        who: {
+            text: "WHO IS MAC",
+            icon: "icon"
+        },
+        work: {
+            text: "PROJECTS",
+            icon: "icon"
+        },
+        contact: {
+            text: "CONTACT",
+            icon: "icon"
+        },
+
+    },
+    es: {}
+};
+
+export const home = {
+    en: {
+        expression: "Hey!",
+        title: "Braulio Baca",
+        subtitle: "aka",
+        akas: [
+            "MacHip3r",
+            "Mac",
+            "Braulio Mac",
+            "BrauMac",
+            "MacBrau"
+        ],
+        positions: [
+            "FullStack Developer",
+            "Computer Systems Engineer",
+            "Programming Technician"
+        ]
+    },
+    es: {}
+};
+
+export const who = {
+    en: {},
+    es: {}
+};
+
+export const work = {
+    en: {},
+    es: {}
+};
+
+export const contact = {
+    en: {},
+    es: {}
+};
+
