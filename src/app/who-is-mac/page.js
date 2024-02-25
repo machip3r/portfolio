2780540f61bfa780a615c4fb309d95b0b9016751
machip3r <mac_258@hotmail.com>
2024-02-25 02:24:36 -0600

    the pages for routing

---
+++ src/app/who-is-mac/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Who = () => {
+    return (
+        <main>
+            <h1>Who</h1>
+        </main>
+    );
+};
+
+export default Who;
---
+++ src/app/work-hard-mac/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Work = () => {
+    return (
+        <main>
+            <h1>Work</h1>
+        </main>
+    );
+};
+
+export default Work;
