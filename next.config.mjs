9d68a1d28cda469149c4dadfa8f44bdf7d43922b
machip3r <mac_258@hotmail.com>
2024-08-12 14:31:10 -0600

    lets test again

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
     basePath: "/portfolio",
     assetPrefix: "/portfolio/",
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
